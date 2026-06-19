2938145e3f91866983c99b29c5e49dccf0f6a6d7
jagans1128-web <jagan.s1128@gmail.com>
2026-06-19 12:21:07 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import htmlimg from "./asscet/html.png"
-import jsimg from "./asscet/download (1).jpg"
-import cssimg from "./asscet/download.jpg"
-
 
 
 import './App.css';
-import Card from './Card';
-import List from "./List";
-import Counter from "./Usestatehook";
-import Useeffecthook from "./Useeffecthook";
-import Userefhook from "./Userefhook";
-import Appp from "./Usememo";
+
+
+
+
 import Todolist from "./Todolist";
 import Practie from "./Practie";
-import Callback from "./Callback";
-import Usereduce from "./Usereduce";
+
+
 
 function App() {
   return (
